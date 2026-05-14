2e495eadb04dcd1df94a07361813b09cae835585
charmina-gem <charmaine.gemdiaz@gmail.com>
2026-05-14 22:11:39 +0400

    Updated

--- Experiment 2 - Paint The Night/sketch.js
+++ Experiment 2 - Paint The Night/sketch.js
@@ -82,15 +82,15 @@ function mouseClicked() {
 
 function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
-  let halfAngle = angle / 2.0;
+  let halfAngle = angle / 2;
   beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius2;
-    let sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
+  for (let i = 0; i < TWO_PI; i += angle) {
+    let starX = x + cos(i) * radius1;
+    let starY = y + sin(i) * radius1;
+    vertex(starX, starY);
+    starX = x + cos(i + halfAngle) * radius2;
+    starY = y + sin(i + halfAngle) * radius2;
+    vertex(starX, starY);
   }
   endShape(CLOSE);
 }
